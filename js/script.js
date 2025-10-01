09c4f9d6069a77223310169f28fad4ca0b7278d6
Michaela <michaela_aulbach@gmx.de>
2025-10-01 14:08:23 +0200

    Anpassung Seitenbreite

--- js/script.js
+++ js/script.js
@@ -8,26 +8,25 @@ function initDataTable() {
 
   try {
     const dt = $(el).DataTable({
+      autoWidth: false,
+      dom:
+        '<"row mb-3"<"col-sm-12 col-md-6"l><"col-sm-12 col-md-6"f>>' +
+        't' +
+        '<"row mt-3"<"col-sm-12 col-md-5"i><"col-sm-12 col-md-7"p>>',
 
-       autoWidth: false,
-  dom:
-    '<"row mb-3"<"col-sm-12 col-md-6"l><"col-sm-12 col-md-6"f>>' +
-    't' +
-    '<"row mt-3"<"col-sm-12 col-md-5"i><"col-sm-12 col-md-7"p>>',
-
-       columns: [
-    { render: $.fn.dataTable.render.text() },                       // Land
-    { render: $.fn.dataTable.render.text() },                       // Unternehmen
-    { render: $.fn.dataTable.render.number(' ', ',', 0, '', '') }   // Emissionen
-  ],
-  search: {
-    regex: false,
-    smart: true,
-    caseInsensitive: true
-  },
-      searching:  true,
-      ordering:   true,
-      order:      [[2, "desc"]],
+      columns: [
+        { render: $.fn.dataTable.render.text() },                       // Land
+        { render: $.fn.dataTable.render.text() },                       // Unternehmen
+        { render: $.fn.dataTable.render.number(' ', ',', 0, '', '') }   // Emissionen
+      ],
+      search: {
+        regex: false,
+        smart: true,
+        caseInsensitive: true
+      },
+      searching: true,
+      ordering: true,
+      order: [[2, "desc"]],
       pageLength: 10,
       lengthMenu: [5, 10, 25, 50],
       columnDefs: [{ targets: 2, type: "num", className: "text-end" }],
@@ -41,11 +40,11 @@ function initDataTable() {
       }
     });
     const $filter = $(el).closest('.dataTables_wrapper').find('div.dataTables_filter input[type="search"]');
-$filter.attr({ inputmode: 'text', maxlength: 200 });
-$filter.on('input', function () {
-  const clean = this.value.replace(/[\u0000-\u001F\u007F]/g, '').trim().slice(0, 200);
-  if (clean !== this.value) this.value = clean;
-});
+    $filter.attr({ inputmode: 'text', maxlength: 200 });
+    $filter.on('input', function () {
+      const clean = this.value.replace(/[\u0000-\u001F\u007F]/g, '').trim().slice(0, 200);
+      if (clean !== this.value) this.value = clean;
+    });
     el.dataset.dtReady = "1";
     window._dt = dt;
     console.log("DataTables (jQuery v1.13.8) initialisiert");
@@ -57,7 +56,7 @@ $filter.on('input', function () {
 document.addEventListener("DOMContentLoaded", () => {
   console.log("[APP] DOM ready");
 
-  const menuBtn   = document.querySelector(".menu-toggle");
+  const menuBtn = document.querySelector(".menu-toggle");
   const menuPanel = document.getElementById("localMenu");
 
   if (menuBtn && menuPanel) {
